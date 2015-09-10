8ec0b3ed6e5b2a6662840be02d607a571c6c4f09
Brendan Benshoof <bbenshoof1@student.cs.gsu.edu>
2015-09-10 11:47:50 -0400

    started changes to enable documentation

--- ipfsApi/client.py
+++ ipfsApi/client.py
@@ -14,6 +14,16 @@ class Client(object):
 
     __client__ = http.HTTPClient
 
+    # BASIC COMMANDS
+    def add(self,path,**flags):
+        """
+
+        """
+        return self._add.prepare(self._client, **self._defaults)(path,*flags)
+
+    def id(self):
+        return self._id.prepare(self._client, **self._defaults)()
+
 
     def __init__(self,
                  host='127.0.0.1',
@@ -21,9 +31,9 @@ class Client(object):
                  base='api/v0',
                  default_enc='json',
                  **defaults):
-        
+
         self._client = self.__client__(host, port, base, default_enc)
-        
+
         # default request keyword-args
         if defaults.has_key('opts'):
             defaults['opts'].update({'encoding': default_enc})
@@ -36,77 +46,59 @@ class Client(object):
         ############
         # COMMANDS #
         ############
-        
+
         # BASIC COMMANDS
-        self.add                = FileCommand('/add')
-        self.cat                =  ArgCommand('/cat')
-        self.ls                 =  ArgCommand('/ls')
-        self.refs               =  ArgCommand('/refs')
-        
-        # DATA STRUCTURE COMMANDS
-        self.block_stat         =  ArgCommand('/block/stat')
-        self.block_get          =  ArgCommand('/block/get')
-        self.block_put          = FileCommand('/block/put',
-                                              accept_multiple=False)
-        self.object_data        =  ArgCommand('/object/data')
-        self.object_links       =  ArgCommand('/object/links')
-        self.object_get         =  ArgCommand('/object/get')
-        self.object_put         = FileCommand('/object/put')
-        self.object_stat        =  ArgCommand('/object/stat')
-        self.object_patch       =  ArgCommand('/object/patch')
-        self.file_ls            =  ArgCommand('/file/ls')
+        self._add                = FileCommand('/add')
+        self._cat                =  ArgCommand('/cat')
+        self._ls                 =  ArgCommand('/ls')
+        self._refs               =  ArgCommand('/refs')
 
-        # ADVANCED COMMANDS
-        self.resolve            =  ArgCommand('/resolve')
-        self.name_publish       =  ArgCommand('/name/publish')
-        self.name_resolve       =     Command('/name/resolve')
-        self.dns                =  ArgCommand('/dns')
-        self.pin_add            =  ArgCommand('/pin/add')
-        self.pin_rm             =  ArgCommand('/pin/rm')
-        self.pin_ls             =     Command('/pin/ls')
-        self.repo_gc            =     Command('/repo/gc')
+        # DAT_A STRUCTURE COMMANDS
+        self._block_stat         =  ArgCommand('/block/stat')
+        self._block_get          =  ArgCommand('/block/get')
+        self._block_put          = FileCommand('/block/put', accept_multiple=False)
+        self._object_data        =  ArgCommand('/object/data')
+        self._object_links       =  ArgCommand('/object/links')
+        self._object_get         =  ArgCommand('/object/get')
+        self._object_put         = FileCommand('/object/put')
+        self._object_stat        =  ArgCommand('/object/stat')
+        self._object_patch       =  ArgCommand('/object/patch')
+        self._file_ls            =  ArgCommand('/file/ls')
 
-        # NETWORK COMMANDS
-        self.id                 =     Command('/id')
-        self.bootstrap          =     Command('/bootstrap')
-        self.swarm_peers        =     Command('/swarm/peers')
-        self.swarm_addrs        =     Command('/swarm/addrs')
-        self.swarm_connect      =  ArgCommand('/swarm/connect')
-        self.swarm_disconnect   =  ArgCommand('/swarm/disconnect')
-        self.swarm_filters_add  =  ArgCommand('/swarm/filters/add')
-        self.swarm_filters_rm   =  ArgCommand('/swarm/filters/rm')
-        self.dht_query          =  ArgCommand('/dht/query')
-        self.dht_findprovs      =  ArgCommand('/dht/findprovs')
-        self.dht_findpeer       =  ArgCommand('/dht/findpeer')
-        self.dht_get            =  ArgCommand('/dht/get',
+        # ADV_ANCED COMMANDS
+        self._resolve            =  ArgCommand('/resolve')
+        self._name_publish       =  ArgCommand('/name/publish')
+        self._name_resolve       =     Command('/name/resolve')
+        self._dns                =  ArgCommand('/dns')
+        self._pin_add            =  ArgCommand('/pin/add')
+        self._pin_rm             =  ArgCommand('/pin/rm')
+        self._pin_ls             =     Command('/pin/ls')
+        self._repo_gc            =     Command('/repo/gc')
+
+        # NET_WORK COMMANDS
+        self._id                 =     Command('/id')
+        self._bootstrap          =     Command('/bootstrap')
+        self._swarm_peers        =     Command('/swarm/peers')
+        self._swarm_addrs        =     Command('/swarm/addrs')
+        self._swarm_connect      =  ArgCommand('/swarm/connect')
+        self._swarm_disconnect   =  ArgCommand('/swarm/disconnect')
+        self._swarm_filters_add  =  ArgCommand('/swarm/filters/add')
+        self._swarm_filters_rm   =  ArgCommand('/swarm/filters/rm')
+        self._dht_query          =  ArgCommand('/dht/query')
+        self._dht_findprovs      =  ArgCommand('/dht/findprovs')
+        self._dht_findpeer       =  ArgCommand('/dht/findpeer')
+        self._dht_get            =  ArgCommand('/dht/get',
                                               decoder='json',
                                               post_hook=lambda r: r[u"Extra"])
         self.dht_put            =  ArgCommand('/dht/put', argc=2)
         self.ping               =  ArgCommand('/ping')
-        
+
         # TOOL COMMANDS
         self.config             =  ArgCommand('/config')
         self.config_show        =     Command('/config/show')
         self.config_replace     =  ArgCommand('/config/replace')
         self.version            =     Command('/version')
 
-
-    def __getattribute__(self, name):
-        """
-        Prepares command request or raises InvalidCommand exception.
-        """
-        try:
-            attr = object.__getattribute__(self, name)
-            if isinstance(attr, Command):
-                return attr.prepare(self._client, **self._defaults)
-            else:
-                return attr
-        except AttributeError:
-            # all non-private attributes are api commands
-            if name[0] != '_':
-                raise InvalidCommand
-            else:
-                raise AttributeError
 
 
     ###########
@@ -120,7 +112,7 @@ class Client(object):
             return res['Hash']
         except:
             return res
-    
+
     def add_json(self, json_obj, **kwargs):
         """Adds a json-serializable Python dict as a json file to IPFS."""
         res = self.add(utils.make_json_buffer(json_obj), **kwargs)
@@ -132,7 +124,7 @@ class Client(object):
     def get_json(self, multihash, **kwargs):
         """Loads a json object from IPFS."""
         return self.cat(multihash, decoder='json', **kwargs)
-        
+
     def add_pyobj(self, py_obj, **kwargs):
         """Adds a picklable Python object as a file to IPFS."""
         res = self.add(utils.make_pyobj_buffer(py_obj), **kwargs)
